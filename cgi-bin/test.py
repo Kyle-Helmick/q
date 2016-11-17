d705631f0b1af86cd4594c234b1243286990484f
mgohde <mgohde10@gmail.com>
2016-11-16 22:44:10 -0700

    Fixed space replacement bug in messages

--- cgi-bin/test.py
+++ cgi-bin/test.py
@@ -2,20 +2,23 @@
 
 import os
 import cgi
+import urllib
 
 def br():
     print '<br>'
 
-
+def p(s):
+    print "<p>%s</p>" % s
 #cgitb.enable()
 
 print("Content-Type: text/html;charset=utf-8")
 print('')
-print("Hello World!")
 br()
 print 'Request Method: ' + os.environ['REQUEST_METHOD']
 br()
 print 'Rsrc requested: ' + os.environ['PATH_INFO']
 br()
+
+p('Query: ' + urllib.unquote(os.environ['QUERY_STRING']))
 
 cgi.print_environ()
